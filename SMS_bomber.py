7d79d04491b16e7ea290a84f487c80d2f7ad971b
lordidris <idris.idrisov100@gmail.com>
2024-12-24 02:53:14 +0300

    Update SMS_bomber.py

--- SMS_bomber.py
+++ SMS_bomber.py
@@ -44,8 +44,8 @@ def send(num, counter, slep):
     req = urllib2.Request(result_url, headers=hdr)
     for x in range(counter):
         banner()
-        #print("Target Number          : 01531999473", num)
-        #print("Number of Message Sent : ", x+1)
+        #print("Target Number          :+905354464007 , num)
+        #print("10 : ", x+1)
         page = urllib2.urlopen(req)
         #resp1=Request(result_url)
         #urlopen(resp1)
